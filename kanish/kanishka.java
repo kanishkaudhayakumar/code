c2110b4b7b1992b03a38951bf1801707f22b03c8
kanishudhya <kanishudhya@gmail.com>
2020-10-12 05:44:54 -0700

    third commit

---
+++ kanish/kanishka.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class kanishka{
+	public static void main(String args[])
+	{
+		System.out.println("welcome all");
+		System.out.println("hello world");
+		System.out.println("hii all");
+		System.out.println("welcome to github");
+                System.out.println("THANK YOU!!!!");	
+	}
+}
+
